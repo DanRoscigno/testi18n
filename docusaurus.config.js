f72ce04103281e83ade0851fcc0d6c94686704e1
DanRoscigno <dan@roscigno.com>
2023-10-15 17:48:25 -0400

    add searchbar
    Signed-off-by: DanRoscigno <dan@roscigno.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -148,6 +148,35 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'ER08SJMRY1',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '08af8d37380974edb873fe8fd61e8dda',
+  
+        indexName: 'starrocks',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        //externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        //replaceSearchResultPathname: {
+          //from: '/en-us/latest/', // or as RegExp: /\/docs\//
+          //to: '/docs/',
+        //},
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        //... other Algolia params
+      },
     }),
 };
 
